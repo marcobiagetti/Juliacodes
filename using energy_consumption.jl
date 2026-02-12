bea63e1ae94bdfcc95ffd5b50e6bfbf49a897ee0
Marco Biagetti <marco.biagetti2013@gmail.com>
2026-02-13 00:36:35 +0100

    Update energy consumption file

--- using energy_consumption.jl
+++ using energy_consumption.jl
@@ -7,3 +7,10 @@ Pkg.add("Plots")
 using Downloads 
 using CSV 
 using DataFrames
+# download a global energy dataset and load it into a DataFrame
+url = "https://raw.githubusercontent.com/owid/energy-data/master/owid-energy-data.csv"
+filename = "owid-energy-data.csv"
+Downloads.download(url, filename)
+energy_data = CSV.read(filename, DataFrame)
+pwd() # check current working directory
+first(energy_data, 5) # preview the first 5 rows of the dataset
